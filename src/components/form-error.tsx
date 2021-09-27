acbff806ac0a9f08c23eb73b3ee926ec1a95d54e
sangjun <juntech0421@github.com>
2021-09-27 19:54:46 +0900

    login mutation & set codegen

---
+++ src/components/form-error.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface IFormErrorProps {
+  errorMessage: string;
+}
+
+export const FormError: React.FC<IFormErrorProps> = ({ errorMessage }) => (
+  <span className="font-normal text-red-600 text-xs">{errorMessage}</span>
+);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,36 +1,60 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState ,useEffect} from "react";
+import { gql, useMutation } from "@apollo/client";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+import { loginMutation, loginMutationVariables } from "../__generated__/loginMutation";
+
+const LOGIN_MUTATION = gql`
+  mutation loginMutation($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      token
+      error
+    }
+  }
+`;
 
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 const LoginPage = () => {
   const {
     register,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     getValues,
     formState: { errors },
     handleSubmit,
   } = useForm<ILoginForm>();
+  const [loginMutation, { data }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION);
   const onSubmit = (data: any) => {
     console.log(data);
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password,
+      },
+    });
   };
-
-  const [redBorder, setRedBorder] = useState(false)
-
-  
+  const [redBorder, setRedBorder] = useState(false);
 
   useEffect(() => {
-    if (errors.password?.type === 'minLength' || errors.password?.message || errors.email?.message) {
-        setRedBorder(true)
-        console.log(redBorder)
+    if (
+      errors.password?.type === "minLength" ||
+      errors.password?.message ||
+      errors.email?.message
+    ) {
+      setRedBorder(true);
     } else {
-        setRedBorder(false)
-        console.log(redBorder)
+      setRedBorder(false);
     }
-  })
+  });
 
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
@@ -46,12 +70,14 @@ const LoginPage = () => {
             required
             type="email"
             placeholder="Email"
-            className={`bg-gray-100 shadow-inner py-3 px-3 rounded-lg focus:outline-none border-2 ${redBorder ? 'focus:border-red-600 focus:outline-none border-2' : 'focus:outline-none border-2 focus:border-green-500 focus:border-opacity-50 '}`}
+            className={`bg-gray-100 shadow-inner py-3 px-3 rounded-lg focus:outline-none border-2 ${
+              redBorder
+                ? "focus:border-red-600 focus:outline-none border-2"
+                : "focus:outline-none border-2 focus:border-green-500 focus:border-opacity-50 "
+            }`}
           />
           {errors.email?.message && (
-            <span className="font-normal text-red-600 text-xs">
-              {errors.email.message}
-            </span>
+            <FormError errorMessage={errors.email?.message} />
           )}
           <input
             {...register("password", {
@@ -62,18 +88,17 @@ const LoginPage = () => {
             required
             type="password"
             placeholder="Password"
-            className={`bg-gray-100 shadow-inner py-3 px-3 rounded-lg ${redBorder ? 'focus:border-red-600 focus:outline-none border-2' : 'focus:outline-none border-2 focus:border-green-500 focus:border-opacity-50 '} `}
+            className={`bg-gray-100 shadow-inner py-3 px-3 rounded-lg ${
+              redBorder
+                ? "focus:border-red-600 focus:outline-none border-2"
+                : "focus:outline-none border-2 focus:border-green-500 focus:border-opacity-50 "
+            } `}
           />
           {errors.password?.message && (
-            <span className="font-normal text-red-600 text-xs">
-              {errors.password.message}
-            </span>
+            <FormError errorMessage={errors.password?.message} />
           )}
           {errors.password?.type === "minLength" && (
-            <span className="font-normal text-red-600 text-xs">
-              비밀번호는 10글자 이상이어야 합니다.
-            </span>
-            
+            <FormError errorMessage={"비밀번호는 10글자 이상이여야 합니다."} />
           )}
           <button className="py-3 px-3 bg-gray-800 text-white rounded-md hover:opacity-80">
             로그인
