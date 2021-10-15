3489d59a0dc4cac5342d4acf33e37d02075e65c2
sangjun <juntech0421@github.com>
2021-10-15 16:42:45 +0900

    e2e test login

---
+++ cypress/integration/Auth/create-account-test.ts
@@ -0,0 +1,5 @@
+describe('회원가입 e2e', () => {
+    it('회원가입 페이지 진입', () => {
+      cy.visit('/create-account')
+    })
+  })
---
+++ cypress/integration/Auth/login-test.ts
@@ -0,0 +1,37 @@
+describe("로그인 e2e", () => {
+  it("홈페이지 진입 & 로그인 페이지 진입", () => {
+    cy.visit("/").title().should("eq", "로그인 : Jun Eats");
+  });
+  it("유효성 검사 에러 메시지가 정상 출력 되는가", () => {
+    cy.visit("/")
+      .get('[name="email"]')
+      .type("test@test")
+      .get(".font-normal")
+      .should("have.text", "유효한 메일 형식을 입력해주세요.")
+      .get('[name="email"]')
+      .clear()
+      .get(".font-normal")
+      .should("have.text", "필수 항목입니다.")
+      .get('[name="password"]')
+      .type("1234")
+      .get(".grid > :nth-child(4)")
+      .should("have.text", "비밀번호는 10글자 이상이여야 합니다.")
+      .get('[name="password"]')
+      .clear()
+      .get(".grid > :nth-child(4)")
+      .should("have.text", "필수 항목입니다.");
+  });
+
+  it("폼 안에 내용을 넣을 수 있는가", () => {
+    cy.visit("/")
+      .get('[name="email"]')
+      .type("qkrtkdwns050@naver.com")
+      .get('[name="password"]')
+      .type("tkdwns1234");
+    cy.findByRole("button")
+      .should("not.have.class", "pointer-event-none")
+      .click();
+    cy.window().its("localStorage.juneats-token").should("be.a", "string");
+  });
+});
+
