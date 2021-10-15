ba48982e1f7ef801926a50270e8f62774e315eb0
sangjun <juntech0421@github.com>
2021-10-15 19:30:46 +0900

    route cleaning

---
+++ src/pages/owner/my-restaurant.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const MyRestaurant = () => {
+    return (
+        <div>my restaurant</div>
+    )
+}
--- src/routes/logged-in-router.tsx
+++ src/routes/logged-in-router.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -12,34 +13,44 @@ import RestaurantDetail from "../pages/client/restaurantDetail";
 import ConfirmEmail from "../pages/user/confirm-email";
 import EditProfile from "../pages/user/edit-profile";
 import VerifyError from "../pages/verify-error";
+import { MyRestaurant } from "../pages/owner/my-restaurant";
 
-const ClientRoutes = [
-  <Route key={1} path="/" exact>
-    <Restaurants />
-  </Route>,
-  <Route key={2} path="/confirm" exact>
-    <ConfirmEmail />
-  </Route>,
-  <Route key={3} path="/verify-error" exact>
-    <VerifyError />
-  </Route>,
-  <Route key={4} path="/edit-profile" exact>
-    <EditProfile />
-  </Route>,
-  <Route key={5} path="/editprofile-error" exact>
-    <EditProfileError />
-  </Route>,
-  <Route key={6} path="/search" exact>
-    <Search />
-  </Route>,
-  <Route key={7} path="/category/:slug" exact>
-    <Category />
-  </Route>,
-
-  <Route key={8} path="/restaurants/:id" exact>
-    <RestaurantDetail />
-  </Route>,
+const commonRoutes = [
+  {
+    path: "/confirm",
+    component: <ConfirmEmail />,
+  },
+  {
+    path: "/edit-profile",
+    component: <EditProfile />,
+  },
 ];
+
+const clientRoutes = [
+  {
+    path: "/",
+    component: <Restaurants />,
+  },
+  { path: "/verify-error", component: <VerifyError /> },
+  {
+    path: "/editprofile-error",
+    component: <EditProfileError />,
+  },
+  {
+    path: "/search",
+    component: <Search />,
+  },
+  {
+    path: "/category/:slug",
+    component: <Category />,
+  },
+  { path: "/restaurants/:id", component: <RestaurantDetail /> },
+];
+
+const OwnerRoutes = [{
+  path: "/",
+  component: <MyRestaurant />
+}];
 
 export const LoggedInRouter = () => {
   const { data, loading, error } = useMe();
@@ -56,7 +67,30 @@ export const LoggedInRouter = () => {
       <Router>
         <Header email={data.me.email} />
         <Switch>
-          {data.me.role === "Client" && ClientRoutes}
+          {data.me.role === "Client" &&
+            clientRoutes.map((route) => {
+              return (
+                <Route key={route.path} path={route.path} exact>
+                  {route.component}
+                </Route>
+              );
+            })}
+          {data.me.role === "Owner" &&
+            OwnerRoutes.map((route) => {
+              return (
+                <Route key={route.path} path={route.path} exact>
+                  {route.component}
+                </Route>
+              );
+            })}
+          {commonRoutes.map((route) => {
+            return (
+              <Route key={route.path} path={route.path} exact>
+                {route.component}
+              </Route>
+            );
+          })}
+
           <Route>
             <NotFound />
           </Route>
