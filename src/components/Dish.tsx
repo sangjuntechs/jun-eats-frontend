c4af864372dcadf377787567fbd696366ea0481a
sangjun <juntech0421@github.com>
2021-11-09 16:18:35 +0900

    dish option & order start

--- src/components/Dish.tsx
+++ src/components/Dish.tsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { restaurant_restaurant_restaurant_menu_option } from "/Users/sangjun/Documents/jun-eats-frontend/src/__generated__/restaurant";
 
 interface IDishProps {
   name: string;
   price: number;
   description: string;
+  isCustomer?: boolean;
+  option?: restaurant_restaurant_restaurant_menu_option[] | null;
 }
 
 export const DishComp: React.FC<IDishProps> = ({
   description,
   name,
   price,
+  isCustomer = false,
+  option,
 }) => {
+  console.log(option)
   return (
-   <div className="flex flex-col p-7 border border-gray-300 hover:border-gray-600 cursor-pointer mb-14 justify-center">
+    <div className="flex flex-col p-7 border border-gray-300 hover:border-gray-600 cursor-pointer mb-14 justify-center">
       <p className="font-medium text-xl">{name}</p>
       <p className="text-sm text-gray-600 font-light">{description}</p>
       <p className="mt-6 font-mono">{price} 원</p>
-  </div>
-);
+      {isCustomer && option?.length !== 0 && <div>
+        <p className='mt-5 font-medium'>음식 옵션</p>
+        {option?.map((opt, index) => {
+        return (
+          <>
+          <span className='text-gray-500 mr-2 flex items-center mt-1' key={index}>
+            <p className='mr-2'>{opt.name}</p>
+            <p className='text-xs'>+{opt.extra}원</p>
+            </span>
+          </>
+        )
+      })}</div>}
+    </div>
+  );
 };
--- src/pages/client/restaurantDetail.tsx
+++ src/pages/client/restaurantDetail.tsx
@@ -19,16 +19,20 @@ const RESTAURANT_QUERY = gql`
       ok
       error
       restaurant {
-          ...RestaurantsPart
-          menu {
-              ...DishesPart
-          }
+        ...RestaurantsPart
+        menu {
+          ...DishesPart
+        }
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
   ${DISH_FRAGMENT}
 `;
+
+const triggerStartOrder = () => {
+  
+}
 
 const RestaurantDetail = () => {
   const params = useParams<IRestaurantProps>();
@@ -66,10 +70,20 @@ const RestaurantDetail = () => {
           </div>
         </div>
       </header>
-      <div className="grid xl:grid-cols-3 xl:max-w-screen-2xl sm:gap-x-2 md:w-full xl:gap-5 mt-20 grid-cols-1 sm:grid-cols-2 gap-x-3 mx-auto w-full">
-          {data?.restaurant.restaurant?.menu.length !== 0 ? data?.restaurant.restaurant?.menu.map((dish) => (
-              <DishComp name={dish.name} description={dish.description} price={dish.price} />
-          )) : "메뉴를 준비 중 입니다."}
+      <button onClick={triggerStartOrder} className='mt-16 ml-3 bg-indigo-600 text-white py-3 px-4 xl:ml-20 hover:bg-indigo-500 transition-colors'>주문 시작</button>
+      <div className="grid xl:grid-cols-3 xl:max-w-screen-2xl sm:gap-x-2 md:w-full xl:gap-5 mt-12 grid-cols-1 sm:grid-cols-2 gap-x-3 mx-auto w-full">
+        {data?.restaurant.restaurant?.menu.length !== 0
+          ? data?.restaurant.restaurant?.menu.map((dish, index) => (
+              <DishComp
+                key={index}
+                name={dish.name}
+                description={dish.description}
+                price={dish.price}
+                isCustomer={true}
+                option={dish.option}
+              />
+            ))
+          : "메뉴를 준비 중 입니다."}
       </div>
     </div>
   );
