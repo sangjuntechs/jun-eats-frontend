17e2b8991d849719ff9a031baa2be6672438bfd3
sangjun <juntech0421@github.com>
2021-11-09 18:43:59 +0900

    order part option add

--- src/components/Dish.tsx
+++ src/components/Dish.tsx
@@ -2,40 +2,90 @@ import React from "react";
 import { restaurant_restaurant_restaurant_menu_option } from "/Users/sangjun/Documents/jun-eats-frontend/src/__generated__/restaurant";
 
 interface IDishProps {
+  id?: number;
   name: string;
   price: number;
   description: string;
   isCustomer?: boolean;
   option?: restaurant_restaurant_restaurant_menu_option[] | null;
   orderStart?: boolean;
+  addItemToOrder?: (dishId: number) => void;
+  isSelected?: boolean;
+  removeFromOrder?: (dishId: number) => void;
+  addOptionToItem?: (dishId: number, option: any) => void;
 }
 
 export const DishComp: React.FC<IDishProps> = ({
+  id = 0,
   description,
   name,
   price,
   isCustomer = false,
   option,
   orderStart = false,
+  addItemToOrder,
+  isSelected = false,
+  removeFromOrder,
+  addOptionToItem,
 }) => {
-  console.log(option)
+  const onClick = () => {
+    if (orderStart) {
+      if (!isSelected && addItemToOrder) {
+        return addItemToOrder(id);
+      }
+      if (isSelected && removeFromOrder) {
+        return removeFromOrder(id);
+      }
+    }
+  };
   return (
-    <div className="flex flex-col p-7 border border-gray-300 hover:border-gray-600 cursor-pointer mb-14 justify-center">
-      <p className="font-medium text-xl">{name}</p>
+    <div
+      className={`flex flex-col p-7 border-gray-300 ${
+        isSelected ? "bg-gray-100 border" : "hover:border-gray-600 border"
+      }  cursor-pointer mb-14 justify-center`}
+    >
+      <p className="font-medium text-xl">
+        {name}
+      </p>
       <p className="text-sm text-gray-600 font-light">{description}</p>
       <p className="mt-6 font-mono">{price} 원</p>
-      {isCustomer && option?.length !== 0 && <div>
-        <p className='mt-5 font-medium'>음식 옵션</p>
-        {option?.map((opt, index) => {
-        return (
-          <>
-          <span className='text-gray-500 mr-2 flex items-center mt-1' key={index}>
-            <p className='mr-2'>{opt.name}</p>
-            <p className='text-xs'>+{opt.extra}원</p>
-            </span>
-          </>
-        )
-      })}</div>}
+      {orderStart && (
+          <button
+            className="px-3 py-1 bg-indigo-600 text-sm font-bold text-white hover:bg-indigo-500 transition-colors w-20 mt-1"
+            onClick={onClick}
+          >
+            {isSelected ? "담기 취소" : "담기"}
+          </button>
+        )}
+      {isCustomer && option?.length !== 0 && (
+        <div>
+          <p className="mt-5 font-medium">음식 옵션</p>
+          {option?.map((opt, index) => {
+            return (
+              <>
+                <span
+                  onClick={() =>
+                    addOptionToItem
+                      ? addOptionToItem(id, {
+                          name: opt.name,
+                        })
+                      : null
+                  }
+                  className={`${
+                    isSelected
+                      ? "bg-indigo-600 px-3 py-1 text-white text-sm inline-block mr-2 mt-1 hover:bg-indigo-500 transition-colors"
+                      : "text-gray-500 mr-2 inline-block items-center mt-1"
+                  }`}
+                  key={index}
+                >
+                  <p className="mr-2">{opt.name}</p>
+                  <p className="text-xs">+{opt.extra}원</p>
+                </span>
+              </>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/client/restaurantDetail.tsx
+++ src/pages/client/restaurantDetail.tsx
@@ -8,6 +8,7 @@ import {
   restaurant,
   restaurantVariables,
 } from "../../__generated__/restaurant";
+import { CreateOrderItemInput } from "../../__generated__/globalTypes";
 
 interface IRestaurantProps {
   id: string;
@@ -30,8 +31,14 @@ const RESTAURANT_QUERY = gql`
   ${DISH_FRAGMENT}
 `;
 
-
-
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($input: CreateOrderInput!) {
+    createOrder(input: $input) {
+      ok
+      error
+    }
+  }
+`;
 
 const RestaurantDetail = () => {
   const params = useParams<IRestaurantProps>();
@@ -44,9 +51,45 @@ const RestaurantDetail = () => {
   });
   const ResName = data?.restaurant.restaurant?.name;
   const [orderStarted, setOrderStarted] = useState(false);
+  const [orderItems, setOrderItems] = useState<CreateOrderItemInput[]>([]);
   const triggerStartOrder = () => {
     setOrderStarted(true);
-  }
+  };
+
+  const getItem = (dishId: number) => {
+    return orderItems.find((order) => order.dishId === dishId);
+  };
+  const isSelected = (dishId: number) => {
+    return Boolean(getItem(dishId));
+  };
+  const addItemToOrder = (dishId: number) => {
+    if (isSelected(dishId)) {
+      return;
+    }
+    setOrderItems((current) => [{ dishId, options:[] }, ...current]);
+  };
+  console.log(orderItems);
+
+  const removeFromOrder = (dishId: number) => {
+    setOrderItems((current) =>
+      current.filter((dish) => dish.dishId !== dishId)
+    );
+  };
+
+  const addOptionToItem = (dishId: number, option: any) => {
+    if (!isSelected(dishId)) {
+      return;
+    }
+
+    const oldItem = getItem(dishId);
+    if (oldItem) {
+      removeFromOrder(dishId);
+      setOrderItems((current) => [
+        { dishId, options: [option, ...oldItem.options!] },
+        ...current,
+      ]);
+    }
+  };
 
   return (
     <div>
@@ -73,18 +116,28 @@ const RestaurantDetail = () => {
           </div>
         </div>
       </header>
-      <button onClick={triggerStartOrder} className='mt-16 ml-3 bg-indigo-600 text-white py-3 px-4 xl:ml-20 hover:bg-indigo-500 transition-colors'>주문 시작</button>
+      <button
+        onClick={triggerStartOrder}
+        className="mt-16 ml-3 bg-indigo-600 text-white py-3 px-4 xl:ml-20 hover:bg-indigo-500 transition-colors"
+      >
+        {orderStarted ? "담는 중" : "메뉴 담기"}
+      </button>
       <div className="grid xl:grid-cols-3 xl:max-w-screen-2xl sm:gap-x-2 md:w-full xl:gap-5 mt-12 grid-cols-1 sm:grid-cols-2 gap-x-3 mx-auto w-full">
         {data?.restaurant.restaurant?.menu.length !== 0
           ? data?.restaurant.restaurant?.menu.map((dish, index) => (
               <DishComp
-                orderStart={orderStarted}
+                isSelected={isSelected(dish.id)}
                 key={index}
+                id={dish.id}
+                orderStart={orderStarted}
                 name={dish.name}
                 description={dish.description}
                 price={dish.price}
                 isCustomer={true}
                 option={dish.option}
+                addItemToOrder={addItemToOrder}
+                removeFromOrder={removeFromOrder}
+                addOptionToItem={addOptionToItem}
               />
             ))
           : "메뉴를 준비 중 입니다."}
